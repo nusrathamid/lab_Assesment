0c8b8bb9099e064c0843454db6534a7b6b8efa09
nusrathamid <87438044+nusrathamid@users.noreply.github.com>
2024-03-04 13:56:45 +0600

    Add files via upload

---
+++ Lab_form/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home Page</title>
+</head>
+<body>
+        <center>
+        <table border="1" width="1500px">
+            <tr>
+                   <td>
+                    <table width="1000px">
+                        <tr>
+                            <td align="Left">
+                                <h1><b>MyCompany</b></h1>
+                            </td>
+                            <td align="Right">
+                                <a href="index.php">Home</a> 
+                                <a href="Login.php">Login</a> 
+                                <a href="Registration.php">Registration</a>
+                        </td>
+                        </tr>
+                    </table>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2" height="150px">
+                    <b>Welcome to MyCompany<b>
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <center>
+                    Copyright 2017
+                    </center>
+                </td>
+            </tr>
+         </table>
+    </center>
+</body>
+</html>
